1d30c8dd8f3534c27014edf86b41fda31a6d5c0d
Jacky Song <dixinlily@hotmail.com>
2021-08-16 21:07:30 -0400

    08/16 progress 1

--- Vecternary-Picasso/scripts/appmenus.js
+++ Vecternary-Picasso/scripts/appmenus.js
@@ -9,30 +9,6 @@
 //   From: "transform opacity-100 scale-100"
 //   To: "transform opacity-0 scale-95"
 
-// toggleMenu function
-// item is the header menu item
-// dropdown is the dropdown menu associated with the menu item
-// function toggleMenu(item, dropdown) {
-//     document.getElementById(item).onclick = function() {
-//         var el = document.getElementById(dropdown);
-//         // If the dropdown menu is not visible
-//         if (el.classList.contains("hidden")) {
-//             // Then we make it visible
-//             el.classList.remove("hidden");
-//             // If the dropdown menu is visible
-//         } else {
-//             // Then we hide it and make it invisible
-//             el.classList.add("hidden")
-//         }
-//         // If focus to the dropdown menu is lost
-//         document.getElementById(item).onblur = function() {
-//             // Then we hide the dropdown menu as well
-//             el.classList.add("hidden")
-//         }
-//     }
-// 
-// }
-
 // File menu implementation
 // wrap things in an IIFE to keep them neatly isolated from other code.
 let createFileDropDown = (function() {
@@ -255,6 +231,27 @@ menuRoot.addEventListener("click", function(event) {
     // }
 
 
-
-
+    // toggleMenu function
+    // item is the header menu item
+    // dropdown is the dropdown menu associated with the menu item
+    // function toggleMenu(item, dropdown) {
+    //     document.getElementById(item).onclick = function() {
+    //         var el = document.getElementById(dropdown);
+    //         // If the dropdown menu is not visible
+    //         if (el.classList.contains("hidden")) {
+    //             // Then we make it visible
+    //             el.classList.remove("hidden");
+    //             // If the dropdown menu is visible
+    //         } else {
+    //             // Then we hide it and make it invisible
+    //             el.classList.add("hidden")
+    //         }
+    //         // If focus to the dropdown menu is lost
+    //         document.getElementById(item).onblur = function() {
+    //             // Then we hide the dropdown menu as well
+    //             el.classList.add("hidden")
+    //         }
+    //     }
+    // 
+    // }
 });
